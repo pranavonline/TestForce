0ef1315bac247e6128dac9edab5aaeecac2eeaac
prana <pranav.smei@hotmail.com>
2023-01-23 00:41:25 -0600

    Updated Code - RestAssured API

--- Test-Auto-Force/src/test/java/common/RestAssuredUtil.java
+++ Test-Auto-Force/src/test/java/common/RestAssuredUtil.java
@@ -1,7 +1,7 @@
 package common;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.equalTo;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -41,7 +41,7 @@ public class RestAssuredUtil {
 		}
 	}
 	
-	public static ValidatableResponse send_request_payload(String requestType, String url,JsonObject payload) {
+	public static  ValidatableResponse send_request_payload(String requestType, String url,JsonObject payload) {
 		switch (requestType.toLowerCase().trim()) {
 		case "post":
 		case "create":
@@ -52,7 +52,7 @@ public class RestAssuredUtil {
 		}
 	}
 	
-	// Validate the requests using below mentioned code
+	//// Validate the requests using below mentioned code
 
 //	public static void main(String[] args) {
 //
@@ -75,6 +75,9 @@ public class RestAssuredUtil {
 //				+ "}";
 //		JsonObject payload =new Gson().fromJson(jsonString, JsonObject.class);
 //		Assert.assertEquals(send_request_payload("POST", url , payload).extract().response().getStatusCode(),200);
-//
+//		send_request_payload("POST", url , payload)
+//			.assertThat()
+//			.statusCode(200)
+//			.body("name", equalTo("The Test Automation Force"));
 //	}
 }
